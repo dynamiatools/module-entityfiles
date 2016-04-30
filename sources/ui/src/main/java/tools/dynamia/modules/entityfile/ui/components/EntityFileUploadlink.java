17f878edbde52d99424c594acd99b1edb238e3fb
Mario Serrano <mario@dynamiasoluciones.com>
2016-04-30 09:32:45 -0500

    mejoras

--- sources/ui/src/main/java/tools/dynamia/modules/entityfile/ui/components/EntityFileUploadlink.java
+++ sources/ui/src/main/java/tools/dynamia/modules/entityfile/ui/components/EntityFileUploadlink.java
@@ -2,9 +2,6 @@ package tools.dynamia.modules.entityfile.ui.components;
 
 import java.io.File;
 
-import org.zkoss.zk.ui.event.Event;
-import org.zkoss.zk.ui.event.Events;
-
 import tools.dynamia.integration.Containers;
 import tools.dynamia.io.FileInfo;
 import tools.dynamia.modules.entityfile.EntityFileException;
@@ -17,88 +14,88 @@ import tools.dynamia.zk.ui.Uploadlink;
 
 public class EntityFileUploadlink extends Uploadlink {
 
-	/**
-	 *
-	 */
-	private static final long serialVersionUID = -2182747459195865750L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = -2182747459195865750L;
 
-	static {
-		BindingComponentIndex.getInstance().put("value", EntityFileUploadlink.class);
-		ComponentAliasIndex.getInstance().put("entityfileUploadlink", EntityFileUploadlink.class);
-	}
+    static {
+        BindingComponentIndex.getInstance().put("value", EntityFileUploadlink.class);
+        ComponentAliasIndex.getInstance().put("entityfileUploadlink", EntityFileUploadlink.class);
+    }
 
-	private EntityFile entityFile;
-	private EntityFileService service = Containers.get().findObject(EntityFileService.class);
-	private boolean shared;
-	private String subfolder;
-	private String storedFileName;
+    private EntityFile entityFile;
+    private EntityFileService service = Containers.get().findObject(EntityFileService.class);
+    private boolean shared;
+    private String subfolder;
+    private String storedFileName;
 
-	public EntityFile getValue() {
-		if (entityFile == null && getUploadedFile() != null) {
-			onFileUpload();
-		}
+    public EntityFile getValue() {
+        if (entityFile == null && getUploadedFile() != null) {
+            onFileUpload();
+        }
 
-		return entityFile;
-	}
+        return entityFile;
+    }
 
-	public void setValue(EntityFile entityFile) {
-		this.entityFile = entityFile;
-		if (entityFile != null) {
-			configureFileInfo();
-		}
-	}
+    public void setValue(EntityFile entityFile) {
+        this.entityFile = entityFile;
+        if (entityFile != null) {
+            configureFileInfo();
+        }
+    }
 
-	private void configureFileInfo() {
-		setUploadedFile(new FileInfo(new File("")));
-		setLabel(entityFile.getName());
-	}
+    private void configureFileInfo() {
+        setUploadedFile(new FileInfo(new File("")));
+        setLabel(entityFile.getName());
+    }
 
-	@Override
-	protected void onFileUpload() {
+    @Override
+    protected void onFileUpload() {
 
-		try {
-			UploadedFileInfo uploadedFileInfo = new UploadedFileInfo(getUploadedFile());
-			uploadedFileInfo.setShared(isShared());
-			uploadedFileInfo.setSubfolder(getSubfolder());
-			if (storedFileName != null && !storedFileName.isEmpty()) {
-				if (storedFileName.equals("real")) {
-					uploadedFileInfo.setStoredFileName(getUploadedFile().getName());
-				} else {
-					uploadedFileInfo.setStoredFileName(storedFileName);
-				}
-			}
-			entityFile = service.createTemporalEntityFile(uploadedFileInfo);
+        try {
+            UploadedFileInfo uploadedFileInfo = new UploadedFileInfo(getUploadedFile());
+            uploadedFileInfo.setShared(isShared());
+            uploadedFileInfo.setSubfolder(getSubfolder());
+            if (storedFileName != null && !storedFileName.isEmpty()) {
+                if (storedFileName.equals("real")) {
+                    uploadedFileInfo.setStoredFileName(getUploadedFile().getName());
+                } else {
+                    uploadedFileInfo.setStoredFileName(storedFileName);
+                }
+            }
+            entityFile = service.createTemporalEntityFile(uploadedFileInfo);
 
-			setLabel(entityFile.getName());
-			
-		} catch (Exception e) {
-			throw new EntityFileException(e);
-		}
+            setLabel(entityFile.getName());
 
-	}
+        } catch (Exception e) {
+            throw new EntityFileException(e);
+        }
 
-	public boolean isShared() {
-		return shared;
-	}
+    }
 
-	public void setShared(boolean shared) {
-		this.shared = shared;
-	}
+    public boolean isShared() {
+        return shared;
+    }
 
-	public String getSubfolder() {
-		return subfolder;
-	}
+    public void setShared(boolean shared) {
+        this.shared = shared;
+    }
 
-	public void setSubfolder(String subfolder) {
-		this.subfolder = subfolder;
-	}
+    public String getSubfolder() {
+        return subfolder;
+    }
 
-	public String getStoredFileName() {
-		return storedFileName;
-	}
+    public void setSubfolder(String subfolder) {
+        this.subfolder = subfolder;
+    }
 
-	public void setStoredFileName(String storedFileName) {
-		this.storedFileName = storedFileName;
-	}
+    public String getStoredFileName() {
+        return storedFileName;
+    }
+
+    public void setStoredFileName(String storedFileName) {
+        this.storedFileName = storedFileName;
+    }
 
 }
